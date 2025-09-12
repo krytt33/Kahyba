853e2ec9b203bb1a84baf0878b85feb4d2e2b67f
DDStepanov <DDStepanov@IVM-14-1009-03D>
2025-09-12 09:33:52 +0300

    второй урок с кашубой попытка

--- Kahyba/lesonN2.cpp
+++ Kahyba/lesonN2.cpp
@@ -3,14 +3,20 @@
 
 using namespace std;
 
-int couti() {
+int cot() {
     int a, b;
-    cin >> a >> b;
-    if ((bool)(a = b)) {
-        a += 7;
-        b -= 2;
+    cin >> a;
+    switch (a) {//множественный выбор
+    case 10://a=10  без брейка
+        a += 7;//10+7=17
+        break;// его нет
+    case 11:
+        a -= 5;//17-5=12
+        break;
+    default:
+        a *= 2;
     }
-    cout << a << " " << b;
+    cout << a;
     return 0;
 }
 
